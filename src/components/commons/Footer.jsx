205787c923e2391f712ac257307712d8f6f2b8a5
Pablo Lires <pablo@librecomunicacion.net>
2025-07-07 21:07:40 -0300

    adecuacion del footer y sus estilos

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -1,4 +1,7 @@
 import logo from '@/assets/img/logo-copiando-byn.webp'
+import iso9001 from '@/assets/img/iso-9001.webp'
+import iso14001 from '@/assets/img/iso-14001.webp'
+import iso27001 from '@/assets/img/iso-27001.webp'
 
 import './footer.css'
 
@@ -7,18 +10,18 @@ const Footer = () => {
     <footer className='container-fluid'>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-3 left'>
-            <img
-              className='logoAfa img-fluid'
-              src={logo}
-              alt='logo afa color'
-            />
+          <div className='col-md-4 contentLogo'>
+            <img className='img-fluid' src={logo} alt='logo footer' />
           </div>
-          <div className='col-md-9 right'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nam
-            corporis neque repellendus illum laborum nesciunt recusandae, esse
-            earum adipisci dignissimos aliquid dolorum dolores ipsam ipsum
-            aliquam excepturi sapiente. Enim?
+          <div className='col-md-8 contentLogosIso'>
+            <img className='img-fluid' src={iso9001} alt='iso9001' />
+            <img className='img-fluid' src={iso14001} alt='iso14001' />
+            <img className='img-fluid' src={iso27001} alt='iso27001' />
+          </div>
+        </div>
+        <div className='row contentYear'>
+          <div className='col-md-12'>
+            All Rights Reserved {new Date().getFullYear()}
           </div>
         </div>
       </div>

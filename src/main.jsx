d7e7bb1fbb43ad503dbea11065f2fd9837f2f045
Pablo Lires <pablo@librecomunicacion.net>
2025-07-22 22:54:47 -0300

    Update. Incorporacion de Context a la aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode, React } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleReCaptchaProvider } from 'react19-google-recaptcha-v3'
+import StoreProvider from './context/store.jsx'
 import App from '@/App.jsx'
 
 import 'normalize.css/normalize.css'
@@ -15,7 +16,9 @@ createRoot(document.getElementById('root')).render(
     <GoogleReCaptchaProvider
       reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY_V3}
     >
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </GoogleReCaptchaProvider>
   </StrictMode>,
 )
